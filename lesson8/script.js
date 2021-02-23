d9db775ec9478c8dd46c1780cc82a7a9be929191
alex0109 <krossyouyub@gmail.com>
2021-02-23 23:38:28 +0200

    commit

---
+++ lesson8/script.js
@@ -0,0 +1,24 @@
+// var date = new Date();
+//
+// console.log(date.getFullYear());
+// console.log(date.getMonth());
+// console.log(date.getDate());
+// console.log(date.getHours());
+// console.log(date.getMinutes());
+// console.log(date.getSeconds());
+// console.log(date.getHours() + ":" + date.getMinutes());
+//
+// date.setHours(23);
+// date.setMinutes(23);
+// console.log(date.getHours() + ":" + date.getMinutes());
+
+
+var arr = [5, 3, 2, 1, 7];
+
+var stroka = arr.reverse().join(", ");
+
+console.log(arr.join(", "));
+console.log(arr.sort());
+console.log(arr.reverse().join(", "));
+
+console.log(stroka.split(", "));
